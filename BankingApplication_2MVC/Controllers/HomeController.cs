47f5cb9711d0c8acb9aa6e7196b9654f2f1c33a7
Praveen <praveen211096@gmail.com>
2023-04-17 11:44:27 +0530

    Added View, Controller and Bootstrap

---
+++ BankingApplication_2MVC/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Repository;
+using System.Dynamic;
+
+namespace BankingApplication_2MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index(String String)
+        //{
+        //    ViewData["name"] = String;
+        //    return View();
+        //}
+
+        public ActionResult Index()
+        {
+            UserRepository urepo = new UserRepository();
+
+            return View(urepo.GetAll());
+        }
+
+    }
+}
